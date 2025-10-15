292b20ed32f5c5dfe777285ac88a613c63643c7a
Pedro Romanato <142935827+Summer761@users.noreply.github.com>
2025-10-14 21:00:56 -0300

    implementacao de verificacoes
    comeco da implementacao do js

---
+++ public/crud inst/crud_inst.js
@@ -0,0 +1,8 @@
+function adicionar(){
+    //funcao que chama todas as verificacoes e acoes a serem feitas.
+    
+}
+function verificar_inputs(){
+    const nome_int=getelementbyid("instituicao-nome");
+    const nome_curso=getelementbyid("instituicao-curso")
+}
