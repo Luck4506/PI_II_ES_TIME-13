e822bf8b463f398f077f87cda0bba8b2783b3413
Pedro Romanato <142935827+Summer761@users.noreply.github.com>
2025-10-31 16:59:46 -0300

    organizando codigo

--- public/instituicao/cadastrar_intituicao/cadastrar_instituica.js
+++ public/instituicao/cadastrar_intituicao/cadastrar_instituica.js
@@ -1,40 +1,52 @@
 
 async function adicionar(){
-    //funcao que chama todas as verificacoes e acoes a serem feitas.
+
     const botao_cadastrar=document.getElementById("btn-cadastrar");
-    //desabilita o botao para nao fazer varias req ao mesmo tempo
+    
+    //desativa clicar o botao novamente.
     botao_cadastrar.disabled = true;
-    //pegando os dados do form.
+
+    //pegar os dados do index
     const nome_int=document.getElementById("instituicao-nome").value.toLowerCase().trim();
     const sigla_int=document.getElementById("instituicao-sigla").value.toLowerCase().trim();
-    //validando os dados enviados.
-    const valido= await verificar_inputs(nome_int,sigla_int,botao_cadastrar);
-    //se for valido manda os dados para o db
-    document.getElementById("instituicao-nome").value = "";
-    document.getElementById("instituicao-sigla").value = "";
+
+    //variavel que verifica a validade dos inputs
+    const valido= await verificar_inputs(nome_int,sigla_int);
+
     if (valido){
+
+        //se tudo der certo faz o cadastro da instituicao
         const realizarCadastro=await cadastrarInstituicao(nome_int,sigla_int);
         if(realizarCadastro){
             console.log("Instituicao cadastrada!");
             alert("Instituicao Cadastrada!");
             botao_cadastrar.disabled = false;
+            limparCampos();
             return;
         }
     }else{
-        console.log("Erro ao cadastrar!");
+
+        //se nao der certo nao faz o cadastro da instituicao
         botao_cadastrar.disabled = false;
+        limparCampos();
         return;
     }
 }
-async function verificar_inputs(nome,sigla,botao){
+async function verificar_inputs(nome,sigla){
+
+    //verifica se os campos estao vazios
     if(nome === ""||sigla===""){
         alert("Preencha todos os campos!");
         return false;
     }
+
+    //verifica se os campos nao tem somente numeros
     if(!isNaN(nome)||!isNaN(sigla)){
         alert("Dados Invalidos!");
         return false;
     }
+
+    //verifica se existe o nome ou sigla existe
     const dados={nome,sigla};
     try{
         const resposta = await fetch('/instituicao/verificar', {
@@ -42,28 +54,29 @@ async function verificar_inputs(nome,sigla,botao){
             headers: {'Content-Type':'application/json'},
             body:JSON.stringify(dados)
         });
+
         if (!resposta.ok){
             alert('Erro ao tentar autenticar.');
             console.warn('HTTP error:', resposta.status, resposta.statusText);
             return;
         }
+        
         const data=await resposta.json();
+
         if (data && data.id){
             alert("Nome ou Sigla ja existe !");
             return false;
         }else{
-            console.log("Todos dados sao validos! (Finalizado verificacao de inputs)");
+            console.log("Todos inputs sao validos!");
             return true;
         }
     }catch (erro){
         console.error('Erro no servidor:',erro);
         return false;
     }
-    //se tudo der certo.
-    return true;
 }
 async function cadastrarInstituicao(nome_int, sigla_int) {
-    const docente_id = 1; // ID fixo de teste (depois pode pegar da sessão)
+    const docente_id = 1; // ID fixo de teste (depois pode pegar da sessão pois e preciso ter ao menos 1 docente)
     const dados2 = { nome: nome_int, sigla: sigla_int, docente_id };
 
     try {
@@ -85,4 +98,9 @@ async function cadastrarInstituicao(nome_int, sigla_int) {
         console.error('Erro no servidor:', erro);
         return false;
     }
+}
+async function limparCampos() {
+    document.getElementById("instituicao-nome").value = "";
+    document.getElementById("instituicao-sigla").value = "";
+    
 }
